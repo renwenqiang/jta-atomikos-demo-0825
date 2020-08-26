1073df547d8b27a454bfa47e13a6565ac1b326de
renwenqiang <1225095213@qq.com>
2020-08-26 09:46:59 +0800

    init commit

---
+++ src/main/java/com/boot/jtaatomikos/JtaAtomikosApplication.java
@@ -0,0 +1,13 @@
+package com.boot.jtaatomikos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtaAtomikosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtaAtomikosApplication.class, args);
+    }
+
+}
