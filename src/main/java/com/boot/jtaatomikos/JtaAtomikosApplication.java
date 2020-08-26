f3b4f24134603245e46921fd3f6ce6d9e7c2c680
renwenqiang <1225095213@qq.com>
2020-08-26 10:15:19 +0800

    按文章上说的配置了，估计没法跑，原作者中间省略了N个文件和步骤，后期抽空我再看看

--- src/main/java/com/boot/jtaatomikos/JtaAtomikosApplication.java
+++ src/main/java/com/boot/jtaatomikos/JtaAtomikosApplication.java
@@ -1,9 +1,13 @@
 package com.boot.jtaatomikos;
 
+import com.boot.jtaatomikos.config.DBConfig1;
+import com.boot.jtaatomikos.config.DBConfig2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = {DBConfig1.class, DBConfig2.class})
 public class JtaAtomikosApplication {
 
     public static void main(String[] args) {
